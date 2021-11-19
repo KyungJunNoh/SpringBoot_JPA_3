296250990beeecabd4b6937c98093804ce89b1e0
노경준 <developerjun0615@gmail.com>
2021-11-19 16:06:06 +0900

    페치조인 - 기본 ( N+1 문제 해결 ) 예제 2

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -30,13 +30,40 @@ public class JpaMain {
             em.flush();
             em.clear();
 
+            String query = "select t from Team t join fetch t.members"; // join fetch 를 써줌으로써 N+1 문제 해결
+
+            List<Team> resultList = em.createQuery(query, Team.class).getResultList();
+
+            for (Team team : resultList) {
+                System.out.println("team = " + team.getName() + ", ");
+                for (Member member : team.getMembers()) {
+                    // 페치 조인으로 팀과 회원을 함께 조회해서 지연 로딩 발생 안함
+                    System.out.println("member = " + member.getUsername());
+                }
+            }
+
+            // 페치조인 - 기본 ( N+1 문제 해결 )
+            /* for (int i = 0; i < 100; i++) { // 100명의 회원정보를 삽입
+                Team team = new Team();
+                team.setName("팀" + i);
+                em.persist(team);
+
+                Member member = new Member();
+                member.setUsername("회원" + i);
+                member.setTeam(team);
+                em.persist(member);
+            }
+
+            em.flush();
+            em.clear();
+
             String query = "select m from Member m join fetch m.team"; // join fetch 를 써줌으로써 N+1 문제 해결
 
             List<Member> resultList = em.createQuery(query, Member.class).getResultList(); // Member를 Select 했을때 이미 Team은 프록시 객체가 아닌 찐 객체로 들어가졌다.
 
             for (Member member : resultList) {
                 System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
-            }
+            }*/
 
             /* Team team = new Team();
             em.persist(team);
